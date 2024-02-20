3aa0ab99e9fb571d60ccd7d788e93120faf62798
Fabio <fabiojosue16@estudiantec.cr>
2024-02-20 17:01:11 -0600

    [ADD] Login Frontend

---
+++ App/App/src/app/pages/login/login.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [MatCardModule,MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  hide = true;
+
+}
