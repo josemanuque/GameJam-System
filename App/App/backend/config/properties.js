fbbb28088df01a3561384c2a01fdee84e5fee9f1
Juleisy Porras <80361927+Juless03@users.noreply.github.com>
2024-02-24 20:10:17 -0600

    [ADD] Database connection

---
+++ App/App/backend/config/properties.js
@@ -0,0 +1,5 @@
+module.exports = {
+    PORT: process.env.PORT || 3000,
+    IP:  '0.0.0.0',
+    DB: 'mongodb://127.0.0.1:27017/gameJamSystem'
+}
--- App/App/backend/index.js
+++ App/App/backend/index.js
@@ -1,11 +1,26 @@
+const mongoose = require('mongoose');
+
 const express = require('express');
+const properties = require('./config/properties');
+const db = require('./config/db');
+
+db();
+
 const app = express();
+const router = express.Router();
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.get('/db-status', (req, res) => {
+    const dbStatus = mongoose.connection.readyState === 1 ? 'Connected' : 'Disconnected';
+    res.send(`Database status: ${dbStatus}`);
+});
+
+app.use(router);
+app.listen(properties.PORT, properties.IP, () => {
+    console.log(`Server is running on port ${properties.PORT}`);
 });
