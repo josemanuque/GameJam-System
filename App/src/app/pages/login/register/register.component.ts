208ee3cb2fea54e24861579902d20e95a4e020e6
Fabio <fabiojosue16@estudiantec.cr>
2024-04-02 22:46:40 -0600

    [UPDATE] Register from page

--- App/src/app/pages/login/register/register.component.ts
+++ App/src/app/pages/login/register/register.component.ts
@@ -10,6 +10,8 @@ import { Router, RouterModule  } from '@angular/router';
 import { NgForm } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
 import {MatMenuModule} from '@angular/material/menu';
+import { SitesService } from '../../../services/sites.service';
+import { SiteListResponseI, SiteResponseI } from '../../../../interfaces/site.interface';
 
 interface Region {
   value: string;
@@ -27,19 +29,32 @@ export class RegisterComponent {
   @ViewChild('usernameInput') usernameInput!: ElementRef;
   @ViewChild('passwordInput') passwordInput!: ElementRef;
 
-  foods: Region[] = [
-    {value: 'steak-0', viewValue: 'North America'},
-    {value: 'pizza-1', viewValue: 'LATAM'},
-    {value: 'tacos-2', viewValue: 'Europe'},
-    {value: 'tacos-3', viewValue: 'Asia'},
-    {value: 'tacos-4', viewValue: 'MENA'},
+  regions: Region[] = [
+    {value: '0', viewValue: 'North America'},
+    {value: '1', viewValue: 'LATAM'},
+    {value: '2', viewValue: 'Europe'},
+    {value: '3', viewValue: 'Asia'},
+    {value: '4', viewValue: 'MENA'},
   ];
+  siteData: SiteResponseI[] = [];
 
   hide = true;
   currentUser: any;
-  constructor(private authService: AuthService, private router: Router){}
+  constructor(private siteService:SitesService, private authService: AuthService, private router: Router){}
 
   ngOnInit(){
+  }
+  getSites(region: string) {
+    this.siteService.getSitesFromRegion(region).subscribe(
+      (response: SiteListResponseI) => {
+        this.siteData = response.sites;
+        console.log('Sites fetched successfully:', this.siteData)
+      },
+      (error) => {
+        console.error('Error occurred while fetching sites:', error);
+        alert('No Sites are created for this region. Please try again.');
+      }
+    );
   }
 
   register(){
