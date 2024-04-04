9bcdafa5c509ee1d95db3bbdb9a697f343738517
Fabio <fabiojosue16@estudiantec.cr>
2024-04-04 00:41:57 -0600

    [ADD] Submit Game

--- App/src/app/pages/dashboard/submit-game/submit-game.component.ts
+++ App/src/app/pages/dashboard/submit-game/submit-game.component.ts
@@ -12,6 +12,11 @@ import { MatChipInputEvent } from '@angular/material/chips';
 import { ENTER, COMMA } from '@angular/cdk/keycodes';
 import { FormArray } from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
+import { CategoryService } from '../../../services/category.service';
+import { TeamService } from '../../../services/team.service';
+import { TeamResponseI } from '../../../../interfaces/team.interface';
+import { GameService } from '../../../services/game.service';
+import { GameRequestI } from '../../../../interfaces/game.interface';
 
 @Component({
   selector: 'app-submit-game',
@@ -23,78 +28,54 @@ import {MatButtonModule} from '@angular/material/button';
 export class SubmitGameComponent {
   form!: FormGroup;
   separatorKeysCodes: number[] = [ENTER, COMMA];
+  categories: any;
+  notInTeam = true;
+  teamData: TeamResponseI | undefined;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder,private catService: CategoryService,private teamService:TeamService, private gameService:GameService) { }
 
   ngOnInit(): void {
     this.form = this.fb.group({
-      gameTitle: ['', Validators.required],
+      title: ['', Validators.required],
       description: ['', Validators.required],
-      teamInfo: ['', Validators.required],
-      gameplayVideo: ['', Validators.required],
-      pitchVideo: ['', Validators.required],
+      teamID: [''],
+      youtubeLinkGameplay: ['', Validators.required],
       buildLink: ['', Validators.required],
-      themes: [[], Validators.required],
+      youtubeLinkPitch: ['', Validators.required],
       categories: [[], Validators.required]
+    });
+    this.catService.getCategoriesName().subscribe(
+      (response) => {
+        this.categories = response.categoryNames;
+      },
+      (error) => {
+        console.error('Error occurred while fetching categories:', error);
+      }
+    );
+    this.teamService.getUserTeam(localStorage.getItem('email')!).subscribe((team) => {
+      if (team) {
+        this.notInTeam = false;
+        this.teamData = team;
+      }
     });
   }
 
-  // Methods to add and remove themes and categories
-  addTheme(event: MatChipInputEvent): void {
-    const input = event.input;
-    const value = event.value.trim();
-
-    // Add theme
-    if (value) {
-      const themes = this.form.get('themes') as FormArray;
-      themes.push(this.fb.control(value));
-    }
-
-    // Reset the input value
-    if (input) {
-      input.value = '';
-    }
-  }
-
-  removeTheme(theme: string): void {
-    const index = this.form.get('themes')!.value.indexOf(theme);
-
-    if (index >= 0) {
-      const themes = this.form.get('themes') as FormArray;
-      themes.removeAt(index);
-    }
-  }
-
-  addCategory(event: MatChipInputEvent): void {
-    const input = event.input;
-    const value = event.value.trim();
-
-    // Add category
-    if (value) {
-      const categories = this.form.get('categories') as FormArray;
-      categories.push(this.fb.control(value));
-    }
-
-    // Reset the input value
-    if (input) {
-      input.value = '';
-    }
-  }
-
-  removeCategory(category: string): void {
-    const index = this.form.get('categories')!.value.indexOf(category);
-
-    if (index >= 0) {
-      const categories = this.form.get('categories') as FormArray;
-      categories.removeAt(index);
-    }
-  }
-
   submitForm(): void {
-    alert('Form submitted successfully!')
     if (this.form.valid) {
-      // Handle form submission
-      console.log('Form submitted successfully!');
+      this.form.patchValue({teamID: this.teamData?._id});
+      const gameData: GameRequestI = this.form.value;
+      console.log('Game data:', gameData);
+      this.gameService.submitGame(gameData).subscribe(
+        (response) => {
+          console.log('Game created successfully:', response);
+          this.form.reset();
+          alert('Game Submited successfully!');
+          window.location.reload();
+        },
+        (error) => {
+          console.error('Error occurred while creating game:', error);
+        }
+      );
     } else {
       // Form is invalid, display error messages
       console.log('Form is invalid!');
