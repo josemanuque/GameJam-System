4bb0ec6953b5c2024c5a06fad51a7485b93f7b72
josemanuque <josemanuque@gmail.com>
2024-04-04 02:19:39 -0600

    Added static express for serving frontend

---
+++ App/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve Static Files
+app.use(express.static(path.join(__dirname, 'dist/app/browser')));
+
+// Get Main Page of Angular App
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/app/browser/index.html'));
+});
+
+
+const PORT = process.env.PORT || 80;
+app.listen(PORT, () => {
+    console.log(`Static express hearing on port ${PORT}`);
+});
