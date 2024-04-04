a8841361a5ea82be51098282b5c1faa501c6e9a0
Fabio <fabiojosue16@estudiantec.cr>
2024-04-03 19:25:05 -0600

    [ADD] View Team

--- App/src/app/pages/dashboard/team/team.component.ts
+++ App/src/app/pages/dashboard/team/team.component.ts
@@ -21,6 +21,8 @@ import {
   MAT_DIALOG_DATA,
 } from '@angular/material/dialog';
 import { Router } from '@angular/router';
+import { TeamService } from '../../../services/team.service';
+import { TeamResponseI } from '../../../../interfaces/team.interface';
 
 @Component({
   selector: 'app-team',
@@ -31,87 +33,20 @@ import { Router } from '@angular/router';
 })
 export class TeamComponent {
   notInTeam = true;
-  constructor(public dialog: MatDialog, private router: Router) {}
+  teamData: TeamResponseI | undefined;
+  constructor(public dialog: MatDialog, private router: Router, private teamService:TeamService) {}
 
-  openDialog(enterAnimationDuration: string, exitAnimationDuration: string): void {
-    const dialogRef = this.dialog.open(DialogTeam, {
-      width: '300px',
-      enterAnimationDuration,
-      exitAnimationDuration,
-      data: { notInTeam: this.notInTeam } // Pass the current value of notInTeam to the dialog
-    });    
-  
-    dialogRef.afterClosed().subscribe(result => {
-      if (result !== undefined && result.notInTeam !== undefined) {
-        this.notInTeam = result.notInTeam;
-        alert('Dialog closed with result: ' + result.notInTeam);
+  ngOnInit(): void {
+    this.teamService.getUserTeam(localStorage.getItem('email')!).subscribe((team) => {
+      if (team) {
+        this.notInTeam = false;
+        this.teamData = team;
+        console.log(this.teamData);
       }
     });
-  }
-  openKickDialog(enterAnimationDuration: string, exitAnimationDuration: string): void {
-    const dialogRef = this.dialog.open(KickDialogTeam, {
-      width: '300px',
-      enterAnimationDuration,
-      exitAnimationDuration,
-      data: { notInTeam: this.notInTeam } // Pass the current value of notInTeam to the dialog
-    });    
   }
 
   goToCreateTeam() {
     this.router.navigate(['/dashboard/team/create']);
-  }
-}
-
-@Component({
-  selector: 'dialog-animations-example-dialog',
-  templateUrl: 'create-team-dialog.html',
-  styleUrl: './create-team-dialog.css',
-  standalone: true,
-  providers: [provideNativeDateAdapter()],
-  imports: [MatInputModule, ReactiveFormsModule, MatFormFieldModule, MatButtonModule, MatDialogActions, MatDialogClose, MatDialogTitle, MatDialogContent, MatDividerModule],
-})
-export class DialogTeam {
-  form!: FormGroup;
-  constructor(public dialogRef: MatDialogRef<DialogTeam>, private fb: FormBuilder, @Inject(MAT_DIALOG_DATA) public data: any) {
-    console.log(data.notInTeam); // Access the value of notInTeam
-  }
-
-  ngOnInit(): void {
-    this.form = this.fb.group({
-      teamName: ['', Validators.required],
-      integrants: ['', Validators.required],
-    });
-  }
-  
-  submitForm(): void {
-    alert('Form submitted successfully!');
-    this.dialogRef.close({ notInTeam: false }); // Close the dialog with the updated value of notInTeam
-  }
-}
-
-@Component({
-  selector: 'dialog-animations-example-dialog2',
-  templateUrl: 'kick-integrant-dialog.html',
-  styleUrl: './kick-integrant-dialog.css',
-  standalone: true,
-  providers: [provideNativeDateAdapter()],
-  imports: [MatInputModule, ReactiveFormsModule, MatFormFieldModule, MatButtonModule, MatDialogActions, MatDialogClose, MatDialogTitle, MatDialogContent, MatDividerModule],
-})
-export class KickDialogTeam {
-  form!: FormGroup;
-  constructor(public dialogRef: MatDialogRef<DialogTeam>, private fb: FormBuilder, @Inject(MAT_DIALOG_DATA) public data: any) {
-    console.log(data.notInTeam); // Access the value of notInTeam
-  }
-
-  ngOnInit(): void {
-    this.form = this.fb.group({
-      teamName: ['', Validators.required],
-      integrants: ['', Validators.required],
-    });
-  }
-  
-  submitForm(): void {
-    alert('Kicked!');
-    this.dialogRef.close({ notInTeam: false }); // Close the dialog with the updated value of notInTeam
   }
 }
